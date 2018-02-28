ce8401632345cf324266caab2fad2ddc9257184b
raxixor <raxixor@gmail.com>
2018-03-01 01:19:49 +0200

    The Documentation Update.

--- SimpleLogCS/Level.cs
+++ SimpleLogCS/Level.cs
@@ -1,28 +1,57 @@
 ﻿namespace SimpleLogCS {
+    /// <summary>
+    /// All of the logging levels used in <see cref="SimpleLog"/>.
+    /// </summary>
     public class Level {
         
+        /// <summary>
+        /// All Logging messages, no matter what.
+        /// </summary>
         public static readonly Level All = new Level("All", 0);
+        /// <summary>
+        /// Trace level, and below
+        /// </summary>
         public static readonly Level Trace = new Level("Trace", 1);
+        /// <summary>
+        /// Debug level, and below
+        /// </summary>
         public static readonly Level Debug = new Level("Debug", 2);
+        /// <summary>
+        /// Info level, and below
+        /// </summary>
         public static readonly Level Info = new Level("Info", 3);
+        /// <summary>
+        /// Warning level, and below
+        /// </summary>
         public static readonly Level Warning = new Level("Warning", 4, true);
+        /// <summary>
+        /// Fatal level, and below
+        /// </summary>
         public static readonly Level Fatal = new Level("Fatal", 5, true);
+        /// <summary>
+        /// Logging is off.
+        /// </summary>
         public static readonly Level Off = new Level("NO-LOGGING", 6, true);
 
-        private readonly string _tag;
-        private readonly int _priority;
-        private readonly bool _isError;
-
         private Level(string tag, int priority, bool isError = false) {
-            _tag = tag;
-            _priority = priority;
-            _isError = isError;
+            GetTag = tag;
+            GetPriority = priority;
+            IsError = isError;
         }
 
-        public string GetTag => _tag;
+        /// <summary>
+        /// Gets the Tag of the Level.
+        /// </summary>
+        public string GetTag { get; }
 
-        public int GetPriority => _priority;
+        /// <summary>
+        /// Gets the Priority of the level.
+        /// </summary>
+        public int GetPriority { get; }
 
-        public bool IsError => _isError;
+        /// <summary>
+        /// Gets whether the level is an error.
+        /// </summary>
+        public bool IsError { get; }
     }
 }
--- SimpleLogCS/SimpleLog.cs
+++ SimpleLogCS/SimpleLog.cs
@@ -4,21 +4,44 @@ using SimpleLogCS.Utilities;
 
 namespace SimpleLogCS {
     
+    /// <summary>
+    /// The class which does everything needed.
+    /// </summary>
     public class SimpleLog {
 
         // Static Level
-        public static Level LEVEL = Level.Info;
+        /// <summary>
+        /// Statically changeable level which is displayed in the Console.
+        /// This can be overridden for every instance of SimpleLog.
+        /// </summary>
+        public static Level GlobalLevel = Level.Info;
 
         // Event Definition
+        /// <summary>
+        /// Handles 'Log' level events (all of them).
+        /// </summary>
+        /// <param name="myObject">The sender of the event</param>
+        /// <param name="args">Event parameters</param>
         public delegate void LogHandler(object myObject, LogArgs args);
+        /// <summary>
+        /// Handles 'Error' level events.
+        /// </summary>
+        /// <param name="myObject"></param>
+        /// <param name="args"></param>
         public delegate void ErrorHandler(object myObject, ErrorArgs args);
 
+        /// <summary>
+        /// Whenever a 'Log' event gets triggered.
+        /// </summary>
         public event LogHandler OnLog;
+        /// <summary>
+        /// Whenever an 'Error' event gets triggered.
+        /// </summary>
         public event ErrorHandler OnError;
 
         // Actual Logging Stuff
-        private static readonly string FORMAT = "[%time%] [%level%] [%name%]: %text%";
-        private static readonly Dictionary<string, SimpleLog> LOGS = new Dictionary<string, SimpleLog>();
+        private const string Format = "[%time%] [%level%] [%name%]: %text%";
+        private static readonly Dictionary<string, SimpleLog> Logs = new Dictionary<string, SimpleLog>();
 
         private readonly string _name;
         private Level _level;
@@ -26,23 +49,41 @@ namespace SimpleLogCS {
             _name = name;
         }
 
+        /// <summary>
+        /// Name of the SimpleLog instance. Used in the format.
+        /// </summary>
         public string Name => _name;
 
+        /// <summary>
+        /// Level of this SimpleLog instance. Overrides the Global level when it comes to Console logging.
+        /// </summary>
         public Level Level {
             get => _level;
             set => _level = value;
         }
 
+        /// <summary>
+        /// Gets a SimpleLog instance with the given name.
+        /// </summary>
+        /// <param name="name">Name of the SimpleLog instance</param>
+        /// <returns>A SimpleLog instance with the given name.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="name"/> is null.</exception>
+        /// <exception cref="System.Exception">Thrown if <paramref name="name"/> is empty.</exception>
         public static SimpleLog GetLog(string name) {
             if (name == null) throw new ArgumentNullException(nameof(name));
             if (name == "") throw new Exception("Name cannot be empty!");
             
-            if (!LOGS.ContainsKey(name.ToLower()))
-                LOGS.Add(name.ToLower(), new SimpleLog(name));
+            if (!Logs.ContainsKey(name.ToLower()))
+                Logs.Add(name.ToLower(), new SimpleLog(name));
 
-            return LOGS[name.ToLower()];
+            return Logs[name.ToLower()];
         }
 
+        /// <summary>
+        /// Logs a message into console, with the given level.
+        /// </summary>
+        /// <param name="level">Level of the message to log.</param>
+        /// <param name="msg">The message to log.</param>
         public void Log(Level level, object msg) {
             OnLog?.Invoke(this, new LogArgs(this, level, msg));
 
@@ -57,10 +98,14 @@ namespace SimpleLogCS {
                     "text", msg.ToString()
                 }
             };
-            var o = new StringSubstitutor(dict).Replace(FORMAT);
+            var o = new StringSubstitutor(dict).Replace(Format);
             Print(o, level);
         }
 
+        /// <summary>
+        /// Logs any Exception with the level of Fatal
+        /// </summary>
+        /// <param name="ex">The Exception to log.</param>
         public void Log(Exception ex) {
             OnError?.Invoke(this, new ErrorArgs(this, ex));
             
@@ -68,28 +113,48 @@ namespace SimpleLogCS {
             Log(Level.Fatal, ex.StackTrace);
         }
 
+        /// <summary>
+        /// Logs a message with the level of Trace
+        /// </summary>
+        /// <param name="msg">The message to log</param>
         public void Trace(object msg) {
             Log(Level.Trace, msg);
         }
 
+        /// <summary>
+        /// Logs a message with the level of Debug
+        /// </summary>
+        /// <param name="msg">The message to log</param>
         public void Debug(object msg) {
             Log(Level.Debug, msg);
         }
 
+        /// <summary>
+        /// Logs a message with the level of Info
+        /// </summary>
+        /// <param name="msg">The message to log</param>
         public void Info(object msg) {
             Log(Level.Info, msg);
         }
 
+        /// <summary>
+        /// Logs a message with the level of Warning
+        /// </summary>
+        /// <param name="msg">The message to log</param>
         public void Warn(object msg) {
             Log(Level.Warning, msg);
         }
 
+        /// <summary>
+        /// Logs a message with the level of Fatal
+        /// </summary>
+        /// <param name="msg">The message to log</param>
         public void Fatal(object msg) {
             Log(Level.Fatal, msg);
         }
 
         private void Print(string msg, Level level) {
-            if (level.GetPriority < (_level?.GetPriority ?? LEVEL.GetPriority))
+            if (level.GetPriority < (_level?.GetPriority ?? GlobalLevel.GetPriority))
                 return;
             
             if (level.IsError) {
@@ -102,34 +167,66 @@ namespace SimpleLogCS {
         }
     }
 
+    /// <summary>
+    /// The arguments for a Log event.
+    /// </summary>
     public class LogArgs : EventArgs {
 
         private readonly SimpleLog _log;
         private readonly Level _level;
         private readonly object _message;
 
+        /// <summary>
+        /// The constructor for the arguments of a Log event.
+        /// </summary>
+        /// <param name="log">The SimpleLog instance which sent the event.</param>
+        /// <param name="level">The Level of the event.</param>
+        /// <param name="message">The message of the event.</param>
         public LogArgs(SimpleLog log, Level level, object message) {
             _log = log;
             _level = level;
             _message = message;
         }
 
+        /// <summary>
+        /// The SimpleLog instance which sent the event.
+        /// </summary>
         public SimpleLog Log => _log;
+        /// <summary>
+        /// The Level of the event.
+        /// </summary>
         public Level Level => _level;
+        /// <summary>
+        /// The Message of the event.
+        /// </summary>
         public object Message => _message;
     }
 
+    /// <summary>
+    /// The arguments for an Error event.
+    /// </summary>
     public class ErrorArgs : EventArgs {
 
         private readonly SimpleLog _log;
         private readonly Exception _exception;
 
+        /// <summary>
+        /// The constructor for the arguments of an Error event.
+        /// </summary>
+        /// <param name="log">The SimpleLog instance which sent the event.</param>
+        /// <param name="exception">The Exception of the event.</param>
         public ErrorArgs(SimpleLog log, Exception exception) {
             _log = log;
             _exception = exception;
         }
 
+        /// <summary>
+        /// The SimpleLog instance which sent the event.
+        /// </summary>
         public SimpleLog Log => _log;
+        /// <summary>
+        /// The Exception of the event.
+        /// </summary>
         public Exception Exception => _exception;
     }
 }
